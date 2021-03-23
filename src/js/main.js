f78af5a13f8327d1c3748a8e019a79f9513e18c4
Nyitrai Bence <nyitrai.done@gmail.com>
2021-03-23 10:33:10 +0100

    Adding jQuery init brackets

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
-console.log( "Hello world! This is " + localize.wp_get_environment_type + " mode." );
+(function ($) {
+    $( function() {
+     
+        console.log( "Hello world! This is " + localize.wp_get_environment_type + " mode." );
 
-if( localize.wp_get_environment_type == 'development'){
-    console.log( "If theres a Violation warning below this, it is only beacause of gulp's browserSync function, it will not appear in production environment." );
-}
+        if( localize.wp_get_environment_type == 'development'){
+            console.log( "If theres a Violation warning below this, it is only beacause of gulp's browserSync function, it will not appear in production environment." );
+        }
+
+    });
+})(jQuery);
